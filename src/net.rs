260d3a0bcc1a705a0a45b4ebcd14b76c7fe5db8c
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-06-13 13:47:43 -0400

    feat(net): use `ci` feature in tests to ignore them during CI builds as GitHub CI cannot handle networking for some reason

--- src/net.rs
+++ src/net.rs
@@ -10,11 +10,13 @@ mod tests {
     use super::*;
 
     #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
     async fn test_is_port_available_unused_port() {
         assert!(is_port_available(3030));
     }
 
     #[tokio::test]
+    #[cfg_attr(feature = "ci", ignore)]
     async fn test_is_port_available_used_port() {
         let listener = TcpListener::bind("localhost:8080").unwrap();
         let port = listener.local_addr().unwrap().port();
