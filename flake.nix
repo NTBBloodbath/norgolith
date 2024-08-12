0a4f046351cf4cfc2d0bc70e7c706c9e1e3eae38
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-08-12 12:18:07 -0400

    ref(nix): ditch rust-overlay, get rust setup by hand
    This is mainly because I was completely unable to tell rust-analyzer how to find the rust-src path to get completions of the stdlib using rust-overlay (it would ignore `RUST_SRC_PATH`)

--- flake.nix
+++ flake.nix
@@ -4,29 +4,58 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [ rustToolchain ];
-          };
-        }
-      );
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem(system: let
+      pkgs = import nixpkgs { inherit system; };
+      toolchain = pkgs.rustPlatform;
+      cargoPackage = ((pkgs.lib.importTOML "${self}/Cargo.toml").package);
+    in rec
+    {
+      # nix build
+      packages.default = toolchain.buildRustPackage {
+        pname = cargoPackage.name;
+        version = cargoPackage.version;
+        src = "${self}";
+        cargoLock.lockFile = "${self}/Cargo.lock";
+
+        meta = {
+          description = cargoPackage.description;
+          homepage = cargoPackage.repository;
+          license = pkgs.lib.licenses.gpl2Only;
+          maintainers = cargoPackage.authors;
+        };
+
+        # For other makeRustPlatform features see:
+        # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
+      };
+
+      # nix run
+      apps.default = flake-utils.lib.mkApp { drv = packages.default; };
+
+      # nix develop
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (with toolchain; [
+            cargo
+            rustc
+            rustLibSrc
+          ])
+          clippy
+          rustfmt
+          cargo-nextest
+          rust-analyzer
+          pkg-config
+        ];
+
+        # Many editors rely on this rust-src PATH variable
+        RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+      };
+    });
 }
