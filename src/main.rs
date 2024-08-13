a773dd090e3969103ec4939cd9ab4bb69fa4350d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-08-12 22:47:56 -0400

    feat: add `converter` mod to source mods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod cli;
 mod cmd;
+mod converter;
 mod fs;
 mod net;
 
@@ -7,6 +8,12 @@ use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // XXX: junk to test the conversion tool
+    //let norg_doc = tokio::fs::read_to_string("/home/amartin/notes/languages/elixir.norg").await?;
+    //let norg_html = converter::convert(norg_doc.clone());
+    //println!("Norg code:{}\n", norg_doc);
+    //println!("HTML code:\n{}", norg_html);
+
     if let Err(e) = cli::start().await {
         eprintln!("Something went wrong: {:?}", e);
         std::process::exit(1);
