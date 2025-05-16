7bb25f9ed02a4e419675cfcb1739d83816b027e2
BoltlessEngineer <boltlessengineer@gmail.com>
2025-05-16 19:21:12 +0900

    feat(preview): `preview` subcommand

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -3,10 +3,12 @@ mod dev;
 mod init;
 mod new;
 mod theme;
+mod preview;
 
 pub use build::build;
 pub use dev::dev;
 pub use init::init;
 pub use new::new;
+pub use preview::preview;
 pub use theme::handle as theme;
 pub use theme::ThemeCommands;
